d08f3fbca52dc6685056fa1a79246ac7eafc1695
uramoia <pksv11@icloud.com>
2024-10-27 02:21:56 +0900

    Update play_audio.js

--- api/play_audio.js
+++ api/play_audio.js
@@ -1,4 +1,10 @@
 export default function handler(req, res) {
-  const { audioFile } = req.query;
-  res.status(200).json({ message: `Playing ${audioFile}` });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    export default function handler(req, res) {
+      const { audioFile } = req.query;
+      res.status(200).json({ message: `Playing ${audioFile}` });
+    }
 }
