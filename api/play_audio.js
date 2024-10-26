e1f50b3380403312cc918c3ad85d42b443821aa6
uramoia <pksv11@icloud.com>
2024-10-27 01:08:12 +0900

    Update play_audio.js

--- api/play_audio.js
+++ api/play_audio.js
@@ -1,21 +1,24 @@
 export default function handler(req, res) {
   const { audioFile } = req.query;
+  console.log("Received request for audioFile:", audioFile);
   
   // 音声ファイルのマッピング
   const audioFiles = {
     'Apple': '/Apple.wav',
-    'Goukei': '/Goukei.wav',
+    'Goukei': '/Goukei.wav'
     'Thanks': '/Thanks.wav',
     'Welcome': '/Welcome.wav',
     'Mikan': 'Mikan.wav'
   };
 
   if (audioFile && audioFiles[audioFile]) {
+    console.log("Playing audio file:", audioFiles[audioFile]);
     res.status(200).json({ 
       message: 'Playing audio', 
       file: audioFiles[audioFile] 
     });
   } else {
+    console.log("Invalid audio file specified:", audioFile);
     res.status(400).json({ 
       message: 'Invalid audio file specified' 
     });
